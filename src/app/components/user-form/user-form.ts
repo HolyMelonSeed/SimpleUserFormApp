39a573b752e21321b640a22cde37fb07b4057c23
HolyMelonSeed <sikora2012alex@gmail.com>
2025-12-03 15:31:56 +0100

    Basic page, basic form, move list-item into user-list folder

--- src/app/components/user-form/user-form.ts
+++ src/app/components/user-form/user-form.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.html',
   styleUrl: './user-form.scss',
 })
 export class UserForm {
-
+OnSubmit() {
+  throw new Error('Method not implemented.');
+}
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    phone: new FormControl('')
+  })
 }
