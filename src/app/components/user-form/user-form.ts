78ba143c2adf10febdb6a7457abc85d41f795562
HolyMelonSeed <sikora2012alex@gmail.com>
2025-12-03 15:51:10 +0100

    Add validation

--- src/app/components/user-form/user-form.ts
+++ src/app/components/user-form/user-form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -12,7 +12,7 @@ OnSubmit() {
   throw new Error('Method not implemented.');
 }
   userForm = new FormGroup({
-    name: new FormControl(''),
-    phone: new FormControl('')
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    phone: new FormControl('', [Validators.required, Validators.pattern("[0-9]{10}")])
   })
 }
