1dc0f98c5ec965eec4058cf98413d37f7030472c
HolyMelonSeed <sikora2012alex@gmail.com>
2025-12-03 15:46:20 +0100

    Basic user list, simple styling

--- src/app/components/user-list/user-list-item/user-list-item.ts
+++ src/app/components/user-list/user-list-item/user-list-item.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { User } from '../../../model/user';
 
 @Component({
   selector: 'app-user-list-item',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './user-list-item.scss',
 })
 export class UserListItem {
-
+removeUser() {
+throw new Error('Method not implemented.');
+}
+  userInputSignal = input<User>();
 }
--- src/app/components/user-list/user-list.ts
+++ src/app/components/user-list/user-list.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { UserListItem } from './user-list-item/user-list-item';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [UserListItem],
   templateUrl: './user-list.html',
   styleUrl: './user-list.scss',
 })
 export class UserList {
+  tempUsers = [
+    {id: 0, name: "Joe", phone: 1231231231, joke: "Joke"},
+    {id: 1, name: "Joe2", phone: 1231231231, joke: "Joke2"},
+    {id: 2, name: "Joe3", phone: 1231231231, joke: "Joke3"},
+  ]
 
+  usersSignal = signal(this.tempUsers)
 }
