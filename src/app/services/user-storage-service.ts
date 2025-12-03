fb538600067598b7e81922ac75ed0eb9c02b66ba
HolyMelonSeed <sikora2012alex@gmail.com>
2025-12-03 16:31:29 +0100

    Implemented joke retrieval

--- src/app/services/user-storage-service.ts
+++ src/app/services/user-storage-service.ts
@@ -1,38 +1,41 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../model/user';
 import { LocalStorageService } from './local-storage-service';
+import { JokeService } from './joke-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserStorageService {
-  userListStorageKey = "userList"
+  userListStorageKey = 'userList';
+  localStorageService = inject(LocalStorageService);
+  jokeService = inject(JokeService);
 
   getUsers() {
-    return this.usersSignal
+    return this.usersSignal;
   }
 
   addUser(name: string, phone: number) {
-    this.usersSignal.update((list) => {
-      const updatedList = [...list, {id: 1, name, phone, joke: "joke"}]
+    this.jokeService.getRandomJoke().subscribe((joke) => {
+      this.usersSignal.update((list) => {
+        const updatedList = [...list, { id: 1, name, phone, joke }];
 
-      this.localStorageService.setItem(this.userListStorageKey, updatedList)
-      return updatedList
-    })
+        this.localStorageService.setItem(this.userListStorageKey, updatedList);
+        return updatedList;
+      });
+    });
   }
 
   deleteUser(user: User) {
     this.usersSignal.update((list) => {
       const updatedList = list.filter((index: User) => {
-        return index != user
-      })
-      
-      this.localStorageService.setItem(this.userListStorageKey, updatedList)
-      return updatedList
-    })
+        return index != user;
+      });
+
+      this.localStorageService.setItem(this.userListStorageKey, updatedList);
+      return updatedList;
+    });
   }
 
-  localStorageService = inject(LocalStorageService)
-
-  usersSignal = signal(this.localStorageService.getItem(this.userListStorageKey))
+  usersSignal = signal(this.localStorageService.getItem(this.userListStorageKey));
 }
